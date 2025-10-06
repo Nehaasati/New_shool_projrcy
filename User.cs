0b398a8bad7eea8e4013f29f99d8a6900013c224
Nehaasati <31asatineha@gmail.com>
2025-10-06 14:02:58 +0200

    Add user class

---
+++ User.cs
@@ -0,0 +1,23 @@
+namespace App
+{
+    using System.Collections.Generic;
+
+    // Class to represent a user
+    public class User
+    {
+        public string Username;
+        public string Password;
+        public List<Item> MyItems;        // User can have many items
+        public List<Trade> MyTrades;      // Trades involving this user
+
+        // Constructor to initialize user
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            MyItems = new List<Item>();
+            MyTrades = new List<Trade>();
+        }
+
+    }
+}
